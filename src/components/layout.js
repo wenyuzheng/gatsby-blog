7bbb91a2ea3646cf0e0193f7b05e59dbcf802126
Wenyu Zheng <zhengwy229@gmail.com>
2023-05-06 14:20:43 +0100

    Follow tutorial to create a blog

---
+++ src/components/layout.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import "./layout.css";
+
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <div className="container">
+      <header>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul className="navLinks">
+          <li className="navLinkItem">
+            <Link to="/" className="navLinkText">
+              Home
+            </Link>
+          </li>
+          <li className="navLinkItem">
+            <Link to="/about" className="navLinkText">
+              About
+            </Link>
+          </li>
+          <li className="navLinkItem">
+            <Link to="/blog" className="navLinkText">
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className="heading">{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/blog/index.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
+import { Link, graphql } from "gatsby";
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+        {data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
+        excerpt
+      }
+    }
+  }
+`;
+
+export const Head = () => <Seo title="My Blog Posts" />;
+
+export default BlogPage;
